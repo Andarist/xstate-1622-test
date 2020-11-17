5ab718d29afcf58d171f67ed9e5143d390044b80
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-11-17 14:19:09 +0100

    Try the test case using @xstate/react from davidkpiano/xstate#1622

---
+++ src/index.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createMachine, assign, spawn } from "xstate";
+import { useActor, useMachine } from "@xstate/react";
+import { ActorRefFrom } from "@xstate/react/lib/types";
+
+const child = createMachine<{ bar: number }, { type: "FOO"; data: number }>({
+  id: "myActor",
+  context: {
+    bar: 1,
+  },
+  initial: "ready",
+  states: {
+    ready: {},
+  },
+});
+
+const m = createMachine<{ actor: ActorRefFrom<typeof child> | null }>(
+  {
+    initial: "ready",
+    context: {
+      actor: null,
+    },
+    states: {
+      ready: {
+        entry: "spawnActor",
+      },
+    },
+  },
+  {
+    actions: {
+      spawnActor: assign({
+        actor: () => spawn(child),
+      }),
+    },
+  }
+);
+
+interface Props {
+  myActor: ActorRefFrom<typeof child>;
+}
+
+function Element({ myActor }: Props) {
+  /**
+   * This is the most important part. I can't live without typed current and send.
+   */
+  const [current, send] = useActor(myActor);
+
+  return (
+    <>
+      {current.context.bar}
+      <div onClick={() => send({ type: "FOO", data: 1 })}>click</div>
+    </>
+  );
+}
+
+function App() {
+  const [current] = useMachine(m);
+
+  if (!current.context.actor) {
+    return null;
+  }
+
+  return <Element myActor={current.context.actor} />;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
